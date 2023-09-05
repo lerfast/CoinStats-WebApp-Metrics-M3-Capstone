0270b0f0d809407433f3b78cb2e2826bbf1f0163
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-09-05 05:58:54 -0500

    Code for testing

---
+++ src/components/test/HomePage.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
+import HomePage from '../HomePage';
+
+// Mockeamos axios
+jest.mock('axios');
+
+describe('HomePage', () => {
+  it('renders without crashing', () => {
+    render(
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
+    );
+
+    // Puedes agregar expect() aquí para hacer assertions
+  });
+
+  it('displays cryptos based on search', async () => {
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
+    );
+
+    const input = getByPlaceholderText('Find Crypto....');
+
+    fireEvent.change(input, { target: { value: 'Bitcoin' } });
+
+    // Debido a que estás mockeando axios, deberás simular la respuesta que esperas de la API. 
+    // Aquí es donde debes ajustar para adaptarlo a tus necesidades.
+    // Por ejemplo:
+    // await waitFor(() => screen.getByText('Bitcoin (BTC)'));
+
+    // Puedes agregar más expect() aquí para hacer assertions
+  });
+});
---
+++ src/components/test/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from '../NavBar';
+
+test('renders NavBar with Home link', async () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
+
+  const homeLink = await screen.findByText('Home');
+  expect(homeLink).toBeInTheDocument();
+});
